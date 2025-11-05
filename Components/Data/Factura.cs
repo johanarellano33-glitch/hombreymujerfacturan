4c3660af4d17d6fa365028ddf69f13054773a3bc
albeeeees13 <antoniocaxxtillo@gmail.com>
2025-11-05 13:35:05 -0700

    agregue las variables de las facturas

---
+++ Components/Data/Factura.cs
@@ -0,0 +1,11 @@
+﻿namespace blazorfactura.Components.Data
+{
+    public class Factura
+    {
+        public int Identificador { get; set; }
+        public DateTime Fecha { get; set; }
+        public string NombreCliente { get; set; }
+        public List<Articulo> Articulos { get; set; } = new List<Articulo>();
+        public decimal Total => Articulos.Sum(a => a.SubTotal);
+    }
+}
