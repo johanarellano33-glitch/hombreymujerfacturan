9588376821482034c14b1904ef712a6fae610944
johanarellano33-glitch <johanarellano33@gmail.com>
2025-11-05 14:17:52 -0700

    aregle lo rojo de las listas

--- Components/Data/ServicioFacturas.cs
+++ Components/Data/ServicioFacturas.cs
@@ -4,29 +4,29 @@ namespace blazorfactura.Components.Data
 {
     public class ServicioFacturas
     {
-        public List<Factura> = new List<Factura>
+        private List<Factura> facturas = new List<Factura>
         {
-           new Factura 
+            new Factura
             {
-            Indentificador = 1,
-             Fecha = DateTime.Now.AddDays(-5),
-            NombreCliente = "Jose",
-            Articulos = new List<Articulo>
-    {
-            new Articulo {Identificador = 1, Nombre = "Mouse", Precio = 1500, Cantidad = 2},
-            new Articulo {Identificador = 2, Nombre = "Teclado", Precio = 1000, Cantidad = 1}
-}
-},
-     new Factura
-     {
-         Identificador = 2,
-         Fecha = DateTime.Now.AddDays(-2),
-         NombreCliente = "Maria",
-         Articulos = new List<Articulo>
+                Identificador = 1,
+                Fecha = DateTime.Now.AddDays(-5),
+                NombreCliente = "Juan Pérez",
+                Articulos = new List<Articulo>
                 {
-                    new Articulo { Identificador = 1, Nombre = "Monitor", Precio = 3500, Cantidad = 1 }
-                    }
-     }
+                    new Articulo { Identificador = 1, Nombre = "Laptop", Precio = 15000, Cantidad = 1 },
+                    new Articulo { Identificador = 2, Nombre = "Mouse", Precio = 1000, Cantidad = 2 }
+                }
+            },
+            new Factura
+            {
+                Identificador = 2,
+                Fecha = DateTime.Now.AddDays(-2),
+                NombreCliente = "María García",
+                Articulos = new List<Articulo>
+                {
+                    new Articulo { Identificador = 1, Nombre = "Teclado", Precio = 1200, Cantidad = 1 }
+                }
+            }
         };
         public Task <List<Factura>> ObtenerFacturas() => Task.FromResult(Facturas);
 {
@@ -47,6 +47,7 @@ public Task EliminarFactura(int identificador)
     }
     return Task.CompletedTask;
 }
+
 }
 }
 
